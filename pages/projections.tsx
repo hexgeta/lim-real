e1aa6efc9e255ef0dbe685bdb1e734a75ee3403b
hexgeta <michael@twospouts.com>
2024-11-13 14:23:42 +0000

    Your descriptive commit message

--- pages/projections.tsx
+++ pages/projections.tsx
@@ -9,7 +9,8 @@ const ProjectionsPage = () => {
   return (
     <div className="p-0">
       <h1 className="text-2xl font-bold mt-10 mb-4 text-center">Projection Charts</h1>
-      <p className="text-white/60 text-center">These projections charts plot the historic market price and underlying HEX stake backing value against one another and project these values out via multipe trend lines. They're useful models to help illustrate how the market value of these tokens can behave over the length of the entire HEX stake.
+      <p className="text-white/60 text-center">
+        These projection charts plot historical market price and underlying HEX stake backing value against one another and then projecting these values into the future via multiple trend lines. They serve as useful models to illustrate how the value of these tokens may behave throughout the length of their HEX stakes.
       </p>
       <div>
         <ProjectionChartMAXI/>
