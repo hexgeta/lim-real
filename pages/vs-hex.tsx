40569d4030917287d292976d4c50ab02f6fe9393
hexgeta <michael@twospouts.com>
2024-11-22 12:54:37 +0000

    Your descriptive commit message

--- pages/vs-hex.tsx
+++ pages/vs-hex.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import PriceComparison from '../components/BtcEthHex';
+import BtcEthPls from '@/components/BtcEthPls';
+import BtcEthPlsx from '@/components/BtcEthPlsx';
+import BtcEthInc from '@/components/BtcEthInc';
 
 const BtcEthHex2 = () => {
   return (
     <div className="p-4">
       <PriceComparison/>
+      <BtcEthPls/>
+      <BtcEthPlsx/>
+      <BtcEthInc/>
     </div>
   );
 };
